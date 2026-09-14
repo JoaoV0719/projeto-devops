2a43847adc52884b1be2e552959842bf790a8bd6
JoaoV0719 <joaovictork2@outlook.com.br>
2026-09-14 19:03:39 -0300

    Outro teste

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/funcaoteste1")
+@app.get("/funcaoteste")
 async def funcaoTeste():
-    return {"teste": True, "num_aleatorio": random.randint(0, 1000)}
+    return {"teste": True, "num_aleatorio": random.randint(0, 20000)}
