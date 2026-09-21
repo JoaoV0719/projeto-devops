041b593a493c7835b5ca059b1226e850d3394da7
JoaoV0719 <joaovictork2@outlook.com.br>
2026-09-21 12:01:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ async def root():
 
 @app.get("/funcaoteste1")
 async def funcaoTeste():
-    return {"teste": True, "num_aleatorio": random.randint(0, 1000)}
+    return {"teste": True, "num_aleatorio": random.randint(0, 5000)}
 
 
 @app.post("/estudantes/cadastro")
